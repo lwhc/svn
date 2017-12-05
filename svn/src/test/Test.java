019726aab6833e0d9d049509bb34f94dcd901d40
aa <aa>
2017-12-05 21:03:54 +0800

    第一次提交

---
+++ svn/src/test/Test.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Test {
+    public static void main(String[] args){
+        int i = 0;
+        Test test = new Test();
+        test.print();
+        i++;
+    }
+
+    public void print(){
+        System.out.println("刘能大战谢广坤");
+    }
+
+}
---
+++ svn/src/test/Test1.java
@@ -0,0 +1,9 @@
+package test;
+
+public class Test1 {
+    public static void main(String[] args){
+        for(int i=0;i<= 10;i++){
+            System.out.println("刘能大战谢广坤");
+        }
+    }
+}
